381909236b5505a062576a64d2df1a5c826e7a9f
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-09-20 13:34:35 -0400

    passes all tests

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(x, y) {
+    return x + y;
+};
+
+function subtract(x, y) {
+    return x - y;
+};
+
+function multiply(x, y) {
+    return x * y;
+};
+
+function divide(x, y) {
+    return x / y;
+};
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    n = parseInt(n, 10);
+    return n;
+}
+
+function preserveDecimal(n) {
+    n = parseFloat(n, 10);
+    return n;
+}
